3ee5616a4734509ea13d5abc9adf32e9887369bd
can-ok <canokan.taskiran@ogr.iu.edu.tr>
2022-10-30 22:33:12 +0300

    create employee and company and add insert service

---
+++ EmployeeManagement/src/test/java/com/canok/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canok.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
